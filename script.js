c6b5a265594ed6e42cc2660bd51086cb0b774548
Larissa Dias <larissadias@MacBook-Pro-de-Vitor.local>
2020-06-09 14:45:18 -0300

    Criar cobrinha

--- script.js
+++ script.js
@@ -1,10 +1,23 @@
 let canvas = document.getElementById ("snake");
 let context = canvas.getContext ("2d");
 let box = 32;
+let snake = [];
+snake [0] = {
+    x: 8 * box,
+    y: 8 * box
+}
 
 function criarBG() {
     context.fillStyle = "lightgreen";
     context.fillRect(0, 0, 16 * box, 16 * box);
 }
 
-criarBG();
+function criarCobrinha(){
+    for(i=0; i<snake.length; i++) {
+        context.fillStyle = "green";
+        context.fillRect(snake[i].x, snake[i].y, box, box);
+    }
+} 
+criarBG ();
+criarCobrinha();
+ 
